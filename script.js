6075ee113bb7417b1bdc884cc063a0b3dbe0ce18
novabrix <197992631+novabrix@users.noreply.github.com>
2025-03-05 17:28:38 -0600

    Made some minor Javascript changes- WIP!

--- script.js
+++ script.js
@@ -7,45 +7,39 @@
     5. Push it all to GitHub and submit ()
 */
 
-console.log ("Hello World!")
+//console.log ("Hello World!")
 
-function getComputerChoice(max){
-    return Math.floor(Math.random()* max)
+let paper = 0;
+let scissors = 1;
+let rock = 2;
+
+
+function getComputerChoice(){
+    return Math.floor(Math.random()* 3)
 }
+let randomValue = getComputerChoice(0, 2);
+console.log(randomValue);
 
-console.log(getComputerChoice(3))
-
-/*function roshambo(){
-    let compAns = null;
-
-    if (getComputerChoice = 0) {
-        compAns = rock;
-    } else if (getComputerChoice = 1){
-        compAns = scissors;
-    } else if (getComputerChoice = 2){
-        compAns = paper;
-    } else {
-        compAns = "Error encountered!";
-    }
-    alert(compAns)
-    return compAns;
-}*/
-
+let text;
 let response = prompt("Throw your hand now!")
 
-function humanResponse(){
-    if (response === null) {
-       let glep = "Error alert! Error alert!"
-    } else if (sign.toLowerCase() === ""){
-       let glep = ("Throw a sign already!! ....Don't be shy!")
-    } else if (sign.toLowerCase() === "rock"){
-       let glep = ("Rock thrown!")
-    } else if (sign.toLowerCase() === "scissors"){
-       let glep = "Scissors thrown!"
-    } else if (sign.toLowerCase() === ""){
-       let glep = "Paper thrown!"
-    }
-    return glep;
+switch(response){
+case " ":
+    text = "Throw a hand already!... Don't be shy!";
+    break;
+ case "rock", "Rock", "ROCK":
+    text = "Rock thrown!";
+    break;
+ case "scissors", "Scissors", "SCISSORS":
+    text = "Scissors thrown!";
+    break;
+ case "paper", "Paper", "PAPER":
+    text = "Paper thrown!";
+    break;
+ default :
+    text = "Error encountered- check your spelling!";
+    break;
 }
+
 
 
