f1c4b2983ae4d993e45ee77400b207c21e9bbec8
novabrix <197992631+novabrix@users.noreply.github.com>
2025-03-29 18:42:06 -0500

    Connected the game to the GUI- console not required~

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
     6. Push it all to GitHub and submit (done)
 
 // Update Time //
-   7. Create three buttons that call the rpsGame function ()
+   7. Create three buttons that call the rpsGame function (done)
    8. Store the scores and declare a winner ()
-   9. Display the results in the box ()
+   9. Display the results in the box (done)
    10. Merge the branches and push to GitHub ()
 */
 
@@ -20,105 +20,94 @@ const paper = 2;
 const scissors = 3;
 
 // Game Starter
-const rBTN = document.getElementsByClassName("one");
-const pBTN = document.getElementsByClassName("two");
-const sBTN = document.getElementsByClassName("three");
+const rBTN = document.querySelector("one");
+const pBTN = document.querySelector("two");
+const sBTN = document.querySelector("three");
 
-const teller = document.getElementsByClassName("teller");
-const scorer = document.getElementsByClassName("scorer");
+const teller = document.querySelector(".teller");
+const scorer = document.querySelector(".scorer");
 
-function startRoundR(){
-   rpsGame ("rock");
+function startRoundR() {
+  rpsGame("rock");
 }
-function startRoundP(){
-   rpsGame ("paper");
+function startRoundP() {
+  rpsGame("paper");
 }
-function startRoundS(){
-   rpsGame ("scissors");
-}
-
-
-function rpsGame(prompter){
-
-// Game Logic
-
-function playRound(humanResponse, computerResponse){
-   let outcome1 = (humanResponse === rock && computerResponse === scissors)
-   let outcome2 = (humanResponse === "rock" && computerResponse === paper)
-   let outcome3 = (humanResponse === "paper" && computerResponse === scissors)
-   let outcome4 = (humanResponse === "paper" && computerResponse === "rock")
-   let outcome5 = (humanResponse === "scissors" && computerResponse === "paper")
-   let outcome6 = (humanResponse === "scissors" && computerResponse === "rock")
-   let outcome7 = (humanResponse === "rock" && computerResponse === "rock")
-   let outcome8 = (humanResponse === "paper" && computerResponse === "paper")
-   let outcome9 = (humanResponse === "scissors" && computerResponse === "scissors")
-   
-   let humanScore = 0;
-   let computerScore = 0;
-   
-   if (humanResponse === computerResponse){
-      console.log(`${humanKeyword} vs. ${computerKeyword}! Tie game!`)
-   } else if (outcome6 || outcome2 || outcome3){
-      console.log(`${computerKeyword} beats ${humanKeyword}! Computer wins!`)
-   } else if (outcome1 || outcome4 || outcome5){
-      console.log(`${humanKeyword} beats ${computerKeyword}! Human wins!`)
-   } else {
-      console.log(`Nobody... wins?`)
-   }
-   }
-
-// Human Response
-
-if (prompter === "rock" || prompter === "Rock" || prompter === "ROCK"){
-   console.log("Rock thrown! (Human)");
-   humanResponse = 1;
-} else if (prompter === "paper" || prompter === "Paper" || prompter === "PAPER"){
-   console.log("Paper thrown! (Human)")
-   humanResponse = 2;
-} else if (prompter === "scissors" || prompter === "Scissors" || prompter === "SCISSORS"){
-   console.log("Scissors thrown! (Human)")
-   humanResponse = 3;
-} else {
-   console.log("Error encountered, check your spelling!")
+function startRoundS() {
+  rpsGame("scissors");
 }
 
-// Computer Response
+function rpsGame(prompter) {
+  // Game Logic
 
-let computerResponse = Math.trunc(Math.random() * 3) + 1;
+  function playRound(humanResponse, computerResponse) {
+    let outcome1 = humanResponse === rock && computerResponse === scissors;
+    let outcome2 = humanResponse === rock && computerResponse === paper;
+    let outcome3 = humanResponse === paper && computerResponse === scissors;
+    let outcome4 = humanResponse === paper && computerResponse === rock;
+    let outcome5 = humanResponse === scissors && computerResponse === paper;
+    let outcome6 = humanResponse === scissors && computerResponse === rock;
 
-if (computerResponse === 1){
-   console.log("Rock thrown! (Computer)")
-} else if (computerResponse === 2){
-   console.log("Paper thrown! (Computer)")
-} else if (computerResponse === 3){
-   console.log("Scissors thrown! (Computer)")
-} else {
-   console.log("Computer error!")
+    let humanScore = 0;
+    let computerScore = 0;
+
+    if (humanResponse === computerResponse) {
+      document.querySelector(
+        ".scorer"
+      ).textContent = `Tie game (${humanThrow} vs. ${computerThrow})!`;
+    } else if (outcome6 || outcome2 || outcome3) {
+      document.querySelector(
+        ".scorer"
+      ).textContent = `Computer wins (${computerThrow} beats ${humanThrow})!`;
+      computerScore++;
+    } else if (outcome1 || outcome4 || outcome5) {
+      document.querySelector(
+        ".scorer"
+      ).textContent = `Human wins! (${humanThrow} beats ${computerThrow})!`;
+      humanScore++;
+    } else {
+      document.querySelector(".scorer").textContent = `Nobody... wins?`;
+    }
+  }
+
+  // Human Response
+
+  if (prompter === "rock") {
+    document.querySelector(".teller").textContent = "Rock thrown! (Human)";
+    humanResponse = 1;
+    humanThrow = "Rock";
+  } else if (prompter === "paper") {
+    document.querySelector(".teller").textContent = "Paper thrown! (Human)";
+    humanResponse = 2;
+    humanThrow = "Paper";
+  } else if (prompter === "scissors") {
+    document.querySelector(".teller").textContent = "Scissors thrown! (Human)";
+    humanResponse = 3;
+    humanThrow = "Scissors";
+  } else {
+    document.querySelector(".teller").textContent =
+      "Error encountered, check your spelling!";
+  }
+
+  // Computer Response
+
+  let computerResponse = Math.trunc(Math.random() * 3) + 1;
+
+  if (computerResponse === 1) {
+    document.querySelector(".tellertwo").textContent =
+      "Rock thrown! (Computer)";
+    computerThrow = "Rock";
+  } else if (computerResponse === 2) {
+    document.querySelector(".tellertwo").textContent =
+      "Paper thrown! (Computer)";
+    computerThrow = "Paper";
+  } else if (computerResponse === 3) {
+    document.querySelector(".tellertwo").textContent =
+      "Scissors thrown! (Computer)";
+    computerThrow = "Scissors";
+  } else {
+    document.querySelector(".tellertwo").textContent = "Computer error!";
+  }
+
+  const roundStorage = playRound(humanResponse, computerResponse);
 }
-
-const roundStorage = playRound(humanResponse, computerResponse);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
