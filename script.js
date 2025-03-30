b1dbb7518f8cf1f190eca05b30b927113c2394cd
novabrix <197992631+novabrix@users.noreply.github.com>
2025-03-30 15:06:56 -0500

    Recorded the scores and displayed them- game fully functional!

--- script.js
+++ script.js
@@ -9,9 +9,10 @@
 
 // Update Time //
    7. Create three buttons that call the rpsGame function (done)
-   8. Store the scores and declare a winner ()
+   8. Store the scores and declare a winner (done)
    9. Display the results in the box (done)
-   10. Merge the branches and push to GitHub ()
+   10. Bug-killer ()
+   11. Merge the branches and push to GitHub ()
 */
 
 // Number Assigner
@@ -27,6 +28,10 @@ const sBTN = document.querySelector("three");
 const teller = document.querySelector(".teller");
 const scorer = document.querySelector(".scorer");
 
+let humanScore = 0;
+let computerScore = 0;
+let winVar = false;
+
 function startRoundR() {
   rpsGame("rock");
 }
@@ -38,76 +43,118 @@ function startRoundS() {
 }
 
 function rpsGame(prompter) {
-  // Game Logic
+  
+  if (humanScore < 5 || computerScore < 5) {
+    // Game Logic
 
-  function playRound(humanResponse, computerResponse) {
-    let outcome1 = humanResponse === rock && computerResponse === scissors;
-    let outcome2 = humanResponse === rock && computerResponse === paper;
-    let outcome3 = humanResponse === paper && computerResponse === scissors;
-    let outcome4 = humanResponse === paper && computerResponse === rock;
-    let outcome5 = humanResponse === scissors && computerResponse === paper;
-    let outcome6 = humanResponse === scissors && computerResponse === rock;
+    function playRound(humanResponse, computerResponse) {
+      let outcome1 = humanResponse === rock && computerResponse === scissors;
+      let outcome2 = humanResponse === rock && computerResponse === paper;
+      let outcome3 = humanResponse === paper && computerResponse === scissors;
+      let outcome4 = humanResponse === paper && computerResponse === rock;
+      let outcome5 = humanResponse === scissors && computerResponse === paper;
+      let outcome6 = humanResponse === scissors && computerResponse === rock;
 
-    let humanScore = 0;
-    let computerScore = 0;
-
-    if (humanResponse === computerResponse) {
-      document.querySelector(
-        ".scorer"
-      ).textContent = `Tie game (${humanThrow} vs. ${computerThrow})!`;
-    } else if (outcome6 || outcome2 || outcome3) {
-      document.querySelector(
-        ".scorer"
-      ).textContent = `Computer wins (${computerThrow} beats ${humanThrow})!`;
-      computerScore++;
-    } else if (outcome1 || outcome4 || outcome5) {
-      document.querySelector(
-        ".scorer"
-      ).textContent = `Human wins! (${humanThrow} beats ${computerThrow})!`;
-      humanScore++;
-    } else {
-      document.querySelector(".scorer").textContent = `Nobody... wins?`;
+      if (humanResponse === computerResponse) {
+        document.querySelector(
+          ".scorer"
+        ).textContent = `🪢Tie game (${humanThrow} vs. ${computerThrow})!`;
+        document.querySelector(
+          ".numberer"
+        ).textContent = `${humanScore} to ${computerScore}`;
+      } else if (outcome6 || outcome2 || outcome3) {
+        document.querySelector(
+          ".scorer"
+        ).textContent = `🤖Computer wins (${computerThrow} beats ${humanThrow})!`;
+        computerScore++;
+        document.querySelector(
+          ".numberer"
+        ).textContent = `${humanScore} to ${computerScore}`;
+      } else if (outcome1 || outcome4 || outcome5) {
+        document.querySelector(
+          ".scorer"
+        ).textContent = `👱🏼Human wins! (${humanThrow} beats ${computerThrow})!`;
+        humanScore++;
+        document.querySelector(
+          ".numberer"
+        ).textContent = `${humanScore} to ${computerScore}`;
+      } else {
+        document.querySelector(".scorer").textContent = `Nobody... wins?`;
+        document.querySelector(
+          ".numberer"
+        ).textContent = `${humanScore} to ${computerScore}`;
+      }
     }
+
+    // Human Response
+
+    if (prompter === "rock") {
+      document.querySelector(".teller").textContent = "Rock thrown! (Human)";
+      humanResponse = 1;
+      humanThrow = "Rock";
+    } else if (prompter === "paper") {
+      document.querySelector(".teller").textContent = "Paper thrown! (Human)";
+      humanResponse = 2;
+      humanThrow = "Paper";
+    } else if (prompter === "scissors") {
+      document.querySelector(".teller").textContent =
+        "Scissors thrown! (Human)";
+      humanResponse = 3;
+      humanThrow = "Scissors";
+    } else {
+      document.querySelector(".teller").textContent =
+        "Error encountered, check your spelling!";
+    }
+
+    // Computer Response
+
+    let computerResponse = Math.trunc(Math.random() * 3) + 1;
+
+    if (computerResponse === 1) {
+      document.querySelector(".tellertwo").textContent =
+        "Rock thrown! (Computer)";
+      computerThrow = "Rock";
+    } else if (computerResponse === 2) {
+      document.querySelector(".tellertwo").textContent =
+        "Paper thrown! (Computer)";
+      computerThrow = "Paper";
+    } else if (computerResponse === 3) {
+      document.querySelector(".tellertwo").textContent =
+        "Scissors thrown! (Computer)";
+      computerThrow = "Scissors";
+    } else {
+      document.querySelector(".tellertwo").textContent = "Computer error!";
+    }
+
+    const roundStorage = playRound(humanResponse, computerResponse);
   }
 
-  // Human Response
+  // Game End Checker
 
-  if (prompter === "rock") {
-    document.querySelector(".teller").textContent = "Rock thrown! (Human)";
-    humanResponse = 1;
-    humanThrow = "Rock";
-  } else if (prompter === "paper") {
-    document.querySelector(".teller").textContent = "Paper thrown! (Human)";
-    humanResponse = 2;
-    humanThrow = "Paper";
-  } else if (prompter === "scissors") {
-    document.querySelector(".teller").textContent = "Scissors thrown! (Human)";
-    humanResponse = 3;
-    humanThrow = "Scissors";
-  } else {
-    document.querySelector(".teller").textContent =
-      "Error encountered, check your spelling!";
+  if (computerScore > 4) {
+    document.querySelector(".teller").textContent = `Game End!`;
+    document.querySelector(".tellertwo").textContent = `🤖`;
+    document.querySelector(
+      ".scorer"
+    ).textContent = `Computer has won the game! ${computerScore} to ${humanScore}`;
+    document.querySelector(".numberer").textContent = `Better luck next time!`;
+    winVar = true;
+  } else if (humanScore > 4) {
+    document.querySelector(".teller").textContent = `Game End!`;
+    document.querySelector(".tellertwo").textContent = `👱🏼`;
+    document.querySelector(
+      ".scorer"
+    ).textContent = `Human has won the game! ${humanScore} to ${computerScore}!`;
+    document.querySelector(".numberer").textContent = `Congratulations!`;
+    winVar = true;
+  } else if (humanScore < 0, computerScore < 0) {
+    document.querySelector(".teller").textContent = `ERROR`;
+    document.querySelector(".tellertwo").textContent = `?`;
+    document.querySelector(
+      ".scorer"
+    ).textContent = `An error has occured! ${computerScore} to ${humanScore}`;
+    document.querySelector(
+      ".numberer"
+    ).textContent = `Reload the page for a possible fix!`;
   }
-
-  // Computer Response
-
-  let computerResponse = Math.trunc(Math.random() * 3) + 1;
-
-  if (computerResponse === 1) {
-    document.querySelector(".tellertwo").textContent =
-      "Rock thrown! (Computer)";
-    computerThrow = "Rock";
-  } else if (computerResponse === 2) {
-    document.querySelector(".tellertwo").textContent =
-      "Paper thrown! (Computer)";
-    computerThrow = "Paper";
-  } else if (computerResponse === 3) {
-    document.querySelector(".tellertwo").textContent =
-      "Scissors thrown! (Computer)";
-    computerThrow = "Scissors";
-  } else {
-    document.querySelector(".tellertwo").textContent = "Computer error!";
-  }
-
-  const roundStorage = playRound(humanResponse, computerResponse);
 }
